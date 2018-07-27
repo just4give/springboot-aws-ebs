edfa5a07c0e797393b1c9e40b96ea7c115ddef12
Mithun Das <mithun.das@coxautoinc.com>
2018-07-27 14:56:39 +0530

    Initial commit

---
+++ src/main/java/com/crazykoder/ebs/SpringbootAwsEbsApplication.java
@@ -0,0 +1,12 @@
+package com.crazykoder.ebs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsEbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsEbsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/crazykoder/ebs/SpringbootAwsEbsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crazykoder.ebs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootAwsEbsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
